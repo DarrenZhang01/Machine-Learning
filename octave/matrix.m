8a8ab64bb2d0cc12a4b5361f9ab5676d9b301f2d
Darren <18633059886@163.com>
2018-05-19 19:38:21 -0400

    basic matrix definition in octave

---
+++ octave/matrix.m
@@ -0,0 +1,16 @@
+% This is a script file for being used to give introduction to matrix in octave
+
+% Define a 3x1 row vector
+v_1 = [1 2 3]
+
+% Define a 1x3 column vector
+v_2 = [1; 2; 3]
+
+% Define a 2x2 square matrix
+v_3 = [1 2; 3 4]
+
+% Define a 5x6 matrix with each entry being 1
+v_4 = ones(5, 6)
+
+% Define a 2x3 matrix with each entry being 6
+v_5 = 6 * ones(2, 3)
