9c492e8679965a6ac164a3b2476635485c3061b5
Darren <18633059886@163.com>
2018-05-19 19:14:16 -0400

    basic octave commands in script

---
+++ octave/basic.m
@@ -0,0 +1,10 @@
+% This is a script file for being used in octave
+% Running this octave script:
+%     1. Open an octave command line interface
+%     2. Direct to the directory that this file exists
+%     3. write "basic"
+%     4. View the result
+ans = 1
+multiplier = 2
+result = ans * multiplier
+result == 2 % The output should be "ans = 1", which means it is true
