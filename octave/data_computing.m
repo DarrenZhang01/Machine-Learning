94ae1f6274c1c79ecd55bc7f0b790f71f5ca6912
Darren <18633059886@163.com>
2018-06-03 22:19:31 -0400

    more operations on matrices and vectors

--- octave/data_computing.m
+++ octave/data_computing.m
@@ -52,7 +52,63 @@ max(A)  % output: [2 2]
 max(V)  % output: 3
 
 % Get the value and the index of the biggest element in V
-[val, ind] = max(V)  % val = 3  ind = 3
+[val, ind] = max(V)  % output: val = 3  ind = 3
 
 % Element level Comparison and assign the result to M
-M = V < 3
+M = V < 3  % output: [1 1 0]
+
+% The indices of the elements that are less than 3 and assign the result to N
+% Note: Index starts from 1
+N = find(V < 3)  % output: [1; 2]
+
+% Use "magic" to create a square matrix such that all the rows and the
+% columns of a matrix sum to the same value
+O = magic(3)  % output: [8 1 6; 3 5 7; 4 9 2]
+
+% Adds up all the elements in a vector
+sum(V)  % output: 6
+
+% Adds up all the columns of a matrix
+sum(A)  % output: [3 3]
+
+% Calculate the product of all the elements in a vector
+prod(V)  % output: 6
+
+% Rounds down all the elements of a matrix
+floor(A)  % output: [1 1; 2 2]
+
+% Rounds up all the elements of a vector
+ceil(V)  % output: [1 2 3]
+
+% Generate a random 2 x 2 matrix and assign it to P
+P = rand(2)  % output: [* *; * *]
+
+% Generate the max in terms of the columns
+Q = max(P, [], 1)
+
+% Generate the max in terms of the rows
+R = max(P, [], 2)
+
+% The maximum element of a matrix
+max(max(A))  % output: 2
+
+% Turn matrix into a vector and assign it to S
+S = A(:)  % output: [1; 2; 1; 2]
+
+% Create a magic 3 x 3 matrix and assign it to T
+T = magic(3)  % output: [8 1 6; 3 5 7; 4 9 2]
+
+% Sum the elements of T in terms of rows
+sum(T, 1)  % output: [15 15 15]
+
+% Sum the elements of T in terms of columns
+sum(T, 2)  % output: [15; 15; 15]
+
+% Multiply each element of T to the according element in a 3 x 3 identity matrix
+M1 = T .* eye(3)  % output: [8 0 0; 0 5 0; 0 0 2]
+
+% Sum of the diagonal elements of T
+sum(sum(M1))  % output: 15
+
+% Sum the other diagonal elements of the matrix T
+sum(sum(T .* flipud(eye(3))))  % output: 15  (flipud represents flip up down)
