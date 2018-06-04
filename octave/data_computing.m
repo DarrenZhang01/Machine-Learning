42dd0051747e3ac7a624425b9591cbf08a8d8bb5
Darren <18633059886@163.com>
2018-06-03 20:10:31 -0400

    Comparison, get the transpose and get the max

--- octave/data_computing.m
+++ octave/data_computing.m
@@ -23,3 +23,36 @@ G = log(A)  % output: [0.00000 0.00000; 0.69315 0.69315]
 
 % base "e" exponentiation to the matrix A and assign the result to H
 H = exp(A)  % output: [2.7183 2.7183; 7.3891 7.3891]
+
+% Take the absolute value of a vector and assign it to V
+V = abs([-1; -2; -3])  % output: [1 2 3]
+
+% Take the negative value of a matrix and assign it to I
+I = -A  % output: [-1 -1; -2 -2]
+
+% Add 1 to every entry of matrix A and assign it to J
+J = A + ones(2, 2)  % output: [2 2; 3 3]
+
+% An equivalent way of adding 1 to each entry of A
+K = A + 1  % output: [2 2; 3 3]
+
+% Look up the current variables
+whos
+
+% Get the transpose of matrix A and assign it to L
+L = A'  % output: [1 2; 1 2]
+
+% Get back it self if transposing twice
+A == L'  % output: [1 1; 1 1]
+
+% Get the biggest element in A
+max(A)  % output: [2 2]
+
+% Get the biggest element in vector V
+max(V)  % output: 3
+
+% Get the value and the index of the biggest element in V
+[val, ind] = max(V)  % val = 3  ind = 3
+
+% Element level Comparison and assign the result to M
+M = V < 3
