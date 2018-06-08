9df9981b2eaf1e9907f2037a1f12d8d5183ce858
Darren <18633059886@163.com>
2018-06-07 21:38:02 -0400

    basic numpy operation

---
+++ numpy/basic.py
@@ -0,0 +1,9 @@
+# Some Practice on Numpy
+# Examples referrence to: "https://www.jianshu.com/p/57e3c0a92f3a"
+import numpy as np
+# 3 x 1 array
+a = np.array([1, 2, 3])
+print(a)
+# 2 x 2 array
+b = np.array([[1, 2], [3, 4]])
+print(b)
