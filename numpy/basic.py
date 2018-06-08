888384e602962d61a81f0bf7e4283a0ff4734f9e
Darren <18633059886@163.com>
2018-06-07 22:01:15 -0400

    matrix of zeros, matrix of ones, constant customized matrix, identity matrix

--- numpy/basic.py
+++ numpy/basic.py
@@ -2,15 +2,37 @@
 # Examples referrence to: "https://www.jianshu.com/p/57e3c0a92f3a"
 # Examples referrence to: "https://zhuanlan.zhihu.com/p/20878530"
 import numpy as np
+
 # 3 x 1 array
 a = np.array([1, 2, 3])
 print("Print out array a:")
 print(a)
 print("The size of the array a:")
 print(a.shape)  # output: (3,) representing it is a 3 x 1 array
+
 # 2 x 2 array
 b = np.array([[1, 2], [3, 4]])
 print("Print out array b:")
 print(b)
 print("The size of the array b:")
 print(b.shape)   # output: (2, 2) representing it is a 2 x 2 array
+
+# Create a 2 x 2 array of zeros
+c = np.zeros((2, 2))
+print("Print out array c:")
+print(c)
+
+# Create a 2 x 2 array of ones
+d = np.ones((2, 2))
+print("Print out array d:")
+print(d)
+
+# Create a customized constant array
+e = np.full((2, 2), 5)
+print("Print out array e:")
+print(e)
+
+# Create a 2 x 2 identity matrix
+f = np.eye(2)
+print("Print out array f:")
+print(f)
