34205f3442916de9170d188d503d3bfe49aa933f
Darren <18633059886@163.com>
2018-06-07 22:03:50 -0400

    revise all the "array" to "matrix"

--- numpy/basic.py
+++ numpy/basic.py
@@ -5,34 +5,34 @@ import numpy as np
 
 # 3 x 1 array
 a = np.array([1, 2, 3])
-print("Print out array a:")
+print("Print out matrix a:")
 print(a)
-print("The size of the array a:")
+print("The size of the matrix a:")
 print(a.shape)  # output: (3,) representing it is a 3 x 1 array
 
 # 2 x 2 array
 b = np.array([[1, 2], [3, 4]])
-print("Print out array b:")
+print("Print out matrix b:")
 print(b)
-print("The size of the array b:")
+print("The size of the matrix b:")
 print(b.shape)   # output: (2, 2) representing it is a 2 x 2 array
 
 # Create a 2 x 2 array of zeros
 c = np.zeros((2, 2))
-print("Print out array c:")
+print("Print out matrix c:")
 print(c)
 
 # Create a 2 x 2 array of ones
 d = np.ones((2, 2))
-print("Print out array d:")
+print("Print out matrix d:")
 print(d)
 
 # Create a customized constant array
 e = np.full((2, 2), 5)
-print("Print out array e:")
+print("Print out matrix e:")
 print(e)
 
 # Create a 2 x 2 identity matrix
 f = np.eye(2)
-print("Print out array f:")
+print("Print out matrix f:")
 print(f)
