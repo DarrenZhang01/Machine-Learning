73ef9b10c453e5be87dfec229c472881306fba10
Darren <18633059886@163.com>
2018-06-07 21:54:50 -0400

    use "shape" to check the size of the array

--- numpy/basic.py
+++ numpy/basic.py
@@ -1,9 +1,16 @@
 # Some Practice on Numpy
 # Examples referrence to: "https://www.jianshu.com/p/57e3c0a92f3a"
+# Examples referrence to: "https://zhuanlan.zhihu.com/p/20878530"
 import numpy as np
 # 3 x 1 array
 a = np.array([1, 2, 3])
+print("Print out array a:")
 print(a)
+print("The size of the array a:")
+print(a.shape)  # output: (3,) representing it is a 3 x 1 array
 # 2 x 2 array
 b = np.array([[1, 2], [3, 4]])
+print("Print out array b:")
 print(b)
+print("The size of the array b:")
+print(b.shape)   # output: (2, 2) representing it is a 2 x 2 array
